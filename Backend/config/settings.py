63ba6dba25c559fe48dd201bdbb25c91584baa70
kennethkimachia <114845663+kennethkimachia@users.noreply.github.com>
2026-04-21 08:49:45 +0300

    Adding log for credential key

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -25,6 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY not found in environment variables!")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
